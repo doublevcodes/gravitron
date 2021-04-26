c33c0119368f4d5a4a67a1e8928f2fc566b1644a
Vivaan Verma <vivaan.verma@gmail.com>
2021-04-26 21:27:21 +0100

    Parametrise integer test

--- tests/tokens/test_integer.py
+++ tests/tokens/test_integer.py
@@ -1,18 +1,25 @@
 from gravitron.tokens.integer import Integer
 import pytest
+import random
+
+test_val_and_base = [[random.randint(1, 10000), random.randint(1, 10000)] for i in range(10)]
 
 
-def test_basic_integer():
+@pytest.mark.parametrize('val, base', test_val_and_base)
+def test_basic_integer(val: int, base: int) -> None:
     integer: Integer = Integer(1, 2)
     assert integer.val == 1
     assert integer.base == 2
+    return
 
 
-def test_invalid_integer_value_on_init():
+def test_invalid_integer_value_on_init() -> None:
     with pytest.raises(TypeError):
         invalid_integer: Integer = Integer('foo', 10)
+    return
 
 
-def test_invalid_integer_base_on_init():
+def test_invalid_integer_base_on_init() -> None:
     with pytest.raises(TypeError):
         invalid_integer: Integer = Integer(10, 'foo')
+    return
